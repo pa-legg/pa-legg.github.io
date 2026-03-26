032db8c5597495ed07b5cc4f3177c0dd9032d120
pa-legg <phillegg@hotmail.com>
2026-03-26 09:29:19 +0000

    Deploying to gh-pages from @ pa-legg/pa-legg.github.io@34bba34409c2ae63e1f4bb56e7ac3c380451d62c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,7 +44,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "post-wazuh-ubuntu-kali-caldera-a-super-quick-way-of-building-a-disposable-cyber-lab",
+        },{id: "post-hacking-the-hdmi-airwaves-side-channel-attacks-to-recover-a-target-display-part-1",
+        
+          title: 'Hacking the HDMI airwaves — side channel attacks to recover a target display... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.open("https://medium.com/@plegg/hacking-the-hdmi-airwaves-side-channel-attacks-to-recover-a-target-display-part-1-7a62aab1789e?source=rss-b5e2ee2ae6ee------2", "_blank");
+          
+        },
+      },{id: "post-wazuh-ubuntu-kali-caldera-a-super-quick-way-of-building-a-disposable-cyber-lab",
         
           title: 'Wazuh, Ubuntu, Kali, Caldera — a super quick way of building a disposable cyber lab... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
@@ -141,17 +152,6 @@ ninja.data = [{
         handler: () => {
           
             window.open("https://medium.com/@plegg/security-data-analytics-05-visualisation-492820fd7cd0?source=rss-b5e2ee2ae6ee------2", "_blank");
-          
-        },
-      },{id: "post-security-data-analytics-04-machine-learning",
-        
-          title: 'Security Data Analytics — 04 — Machine Learning <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@plegg/security-data-analytics-04-machine-learning-5edef0e764f0?source=rss-b5e2ee2ae6ee------2", "_blank");
           
         },
       },{id: "post-a-post-with-plotly-js",
