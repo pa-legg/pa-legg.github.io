22a1a66423bc50c1a5def8074fc053fd55f3b6e8
pa-legg <phillegg@hotmail.com>
2026-06-17 17:34:58 +0000

    Deploying to gh-pages from @ pa-legg/pa-legg.github.io@50846f6ba55becf170a0d9e6ea9d8ad80b4099af 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "93 publications (up to June 2026) in reversed chronological order.",
+          description: "94 publications (up to June 2026) in reversed chronological order.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
